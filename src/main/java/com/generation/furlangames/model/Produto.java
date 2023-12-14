a8aa4b47b3ca4a52a8031f5b5e047f564acbab43
unknown <richardsonfurlan@hotmail.com>
2023-12-14 17:27:50 -0300

    Criando MVC e Crud de Produto e Categoria

---
+++ src/main/java/com/generation/furlangames/model/Produto.java
@@ -0,0 +1,74 @@
+package com.generation.furlangames.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres!")
+	@Column(length = 100)
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo descrição deve conter no mínimo 05 e no máximo 100 caracteres!")
+	@Column(length = 1000)
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+	
+}
