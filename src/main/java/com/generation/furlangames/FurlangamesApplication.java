f0e622dc0ea34db9dc029c390624fe7c54cbede8
unknown <richardsonfurlan@hotmail.com>
2023-12-14 11:26:41 -0300

    Criando o Projeto

---
+++ src/main/java/com/generation/furlangames/FurlangamesApplication.java
@@ -0,0 +1,13 @@
+package com.generation.furlangames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurlangamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurlangamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/furlangames/FurlangamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.furlangames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurlangamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
